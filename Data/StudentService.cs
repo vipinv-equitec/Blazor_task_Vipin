1b2c793c1d2ed3b00cca1472ee30e9bf96a99d60
vipinv-equitec <156300849+vipinv-equitec@users.noreply.github.com>
2024-01-15 20:31:54 +0530

    init

---
+++ Data/StudentService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BlazorApp.Data
+{
+    public class StudentService
+    {
+        private readonly StudentContext _dbContext;
+
+        public StudentService(StudentContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Student>> GetStudentData()
+        {
+            List<Student> students = await _dbContext.Students.ToListAsync();
+            return students ?? new List<Student>();
+        }
+
+    }
+}
